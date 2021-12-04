c4a4a8e9df23b4eabeb1218bbbe2103c563356e9
Velds <nicholas_matthew1@yahoo.com>
2021-12-05 01:39:33 +0800

    tailwind css setup

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Getting Started with Create React App
-
+# Checking if i can push 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-full w-full">
+      <div className="bg-blue-900 h-full w-full">
+        <button className="bg-blue-500 text-white font-bold rounded px-2 py-2 hover:bg-blue-400">Testing</button>
+      </div>
     </div>
   );
 }
-
 export default App;
